ab1342b3365e5c917290aec0cd4236d5a7b2a88d
lucasgls <lucasglsilva7@gmail.com>
2025-08-12 10:06:17 -0300

    refactor: move header component to separate file and improve structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,13 @@
 
-import lucasLogo from './assets/icon-face-whiteBG.png'
 import './App.css'
 import { Linkedin, Github, Instagram, FolderKanban, Microchip, GraduationCap, PersonStanding, Copy} from 'lucide-react';
+import { Header } from './components/header';
 
 function App() {
 
   return (
     <>
-      <div className='header'>
-
-        <a href="https://www.instagram.com/lucassgls/" target="_blank">
-          <img src={lucasLogo} className="logo" alt="React logo" />
-        </a>
-
-      </div>
-
-      <div className='bio-header'>
-
-        <h1>Lucas Gabriel Lima Silva</h1>
-        <p>Estudante de Engenharia de Software</p>
-
-        <p>
-          <a href="https://www.alura.com.br/artigos/backend?srsltid=AfmBOorHIurbTU-VeUMK_uNDc7URi0UyhLlSD8AVWeMf_j1eYFgNifek" target='_blank'>
-            <u> Backend</u>
-          </a>
-          | .Net | C# | ASP.NET Core
-        </p>
-
-      </div>
-      <div className='social'>
-
-        <div className='linkedin'>
-          <a href="https://www.linkedin.com/in/lucasgls/" target='_blank'>
-            <Linkedin />
-          </a>
-        </div>
-
-        <div className='github'>
-          <a href="https://github.com/lucasgls" target='_blank'>
-            <Github />
-          </a>
-        </div>
-
-        <div className='instagram'>
-          <a href="https://www.instagram.com/lucassgls/" target='_blank'>
-            <Instagram />
-          </a>
-        </div>
-
-      </div>
+      <Header />
 
       <div className="container">
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,34 +1,45 @@
-import lucasLogo from 'src/assets/icon-face-whiteBG.png'
+import lucasLogo from '../assets/icon-face-whiteBG.png'
+import { Linkedin, Github, Instagram } from 'lucide-react';
 
 export function Header () {
-    const logo = lucasLogo;  
     return(
-    <div className="header">
-        <div>
-            <a href="https://www.linkedin.com/in/lucasgls/" target="_blank">
-                <img src={logo} className="logo" alt="Vite logo"/>
+    <>
+        <div className="header">
+            <a href="https://www.instagram.com/lucassgls/" target="_blank">
+                <img src={lucasLogo} className="logo" alt="Lucas logo"/>
             </a>
         </div>
 
-        <div>
+        <div className='bio-header'>
             <h1>Lucas Gabriel Lima Silva</h1>
             <p>Estudante de Engenharia de Software</p>
-            <p> <u>BackEnd</u> | .NET | C# | ASP.NET Core</p>
+            <p>
+                <a href="https://www.alura.com.br/artigos/backend?srsltid=AfmBOorHIurbTU-VeUMK_uNDc7URi0UyhLlSD8AVWeMf_j1eYFgNifek" target='_blank'>
+                    <u>Backend</u>
+                </a>
+                | .NET | C# | ASP.NET Core
+            </p>
         </div>
 
-        <div className='socials-buttons-header'>
-        
-            <div>
-                <button >Projetos</button>
-                <button >Tecnologias</button>
+        <div className='social'>
+            <div className='linkedin'>
+                <a href="https://www.linkedin.com/in/lucasgls/" target='_blank'>
+                    <Linkedin />
+                </a>
             </div>
 
-            <div>
-                <button >Estudos</button>
-                <button >Biografia</button>
+            <div className='github'>
+                <a href="https://github.com/lucasgls" target='_blank'>
+                    <Github />
+                </a>
             </div>
-        
+
+            <div className='instagram'>
+                <a href="https://www.instagram.com/lucassgls/" target='_blank'>
+                    <Instagram />
+                </a>
+            </div>
         </div>
-    </div>
+    </>    
     )
 }
